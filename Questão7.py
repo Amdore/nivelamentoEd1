27b083e6359114d7cb16c66227e6f351a12d3327
André Pimentel <94861303+Amdore@users.noreply.github.com>
2022-10-08 23:07:27 -0300

    Create Questão7.py

---
+++ Questão7.py
@@ -0,0 +1,15 @@
+queue = []
+queue.append('Musica 1')
+queue.append('Musica 2')
+queue.append('Musica 3')
+	
+print(queue)
+
+while queue:
+  queue.pop(0)
+	
+print(queue)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
